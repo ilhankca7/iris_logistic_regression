717f3b8d8a04f5c497eda9ca89a64adde00a20ba
ilhankca7 <106533463+ilhankca7@users.noreply.github.com>
2024-05-08 15:31:40 +0300

    iris_logistic_regression
    Bu kod bloğu, bir Lojistik Regresyon sınıflandırma modelini eğitir, bu modelin performansını değerlendirir (doğruluk, çapraz doğrulama skoru ve karışıklık matrisi kullanarak) ve veri setinin bir kısmını eğitim için, diğer kısmını test etmek için kullanır.

---
+++ logisticregresyon2.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder,StandardScaler
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix,accuracy_score
+data=pd.read_csv("C:/Users/ilhan/Downloads/iris.csv")
+veri=data.copy()
+
+veri=veri.drop(columns="Id",axis=1)
+le=LabelEncoder()
+veri["Species"]=le.fit_transform(veri["Species"])
+
+y=veri["Species"]
+X=veri.drop(columns="Species",axis=1)
+print(X)
+
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+
+
+model=LogisticRegression(random_state=0)
+model.fit(X_train,y_train)
+tahmin=model.predict(X_test)
+print(tahmin)
+
+cm=confusion_matrix(y_test,tahmin)
+acs=accuracy_score(y_test,tahmin)
+print(cm)
+print(acs)
+
+
+cv=cross_val_score(model,X_test,y_test,cv=10).mean()
+print(cv)
+
+
+
+
+
+
+
